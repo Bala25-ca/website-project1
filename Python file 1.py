f86230c88b853fdcddfc8a6ca68d5712979594ef
Bala25-ca <cbaladevi@gmail.com>
2026-05-15 01:55:17 -0700

    Python file added to repo

---
+++ Python file 1.py
@@ -0,0 +1,8 @@
+
+print ("hello world")
+print ("welcome to python programming")
+print ("python is a great language")
+name = input("what is your name? ")
+print ("hello " + name + ", nice to meet you!")
+age = input("how old are you? ")
+print ("you are " + age + " years old.")
